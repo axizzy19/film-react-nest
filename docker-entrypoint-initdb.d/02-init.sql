0f9a74463b7369dd41b3327db2432cad84273857
Alexandra Akhundova <alexsandra.akhundova@yandex.ru>
2025-03-13 22:10:03 +0300

    some changes 5

--- docker-entrypoint-initdb.d/02-init.sql
+++ docker-entrypoint-initdb.d/02-init.sql
@@ -1,9 +1,9 @@
 -- Создание базы данных
--- CREATE DATABASE prac;
--- CREATE USER prac PASSWORD prac;
--- GRANT ALL PRIVILEGES ON DATABASE prac TO prac;
+CREATE DATABASE prac;
+CREATE USER prac PASSWORD prac;
+GRANT ALL PRIVILEGES ON DATABASE prac TO prac;
 
-\connect prac;
+\connect prac
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
